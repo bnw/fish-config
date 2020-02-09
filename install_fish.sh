1a861c6a5b991d13ccb187cef95b22480fda3f0f
Benjamin Klotz <bnw@users.noreply.github.com>
2020-02-09 17:44:52 +0100

    Create install_fish.sh

---
+++ install_fish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+set -e
+
+sudo apt-get install fish git
+curl -L https://get.oh-my.fish > install
+fish ./install --noninteractive
+rm install
+fish -c "
+omf install bobthefish
+set -g theme_nerd_fonts yes
+cd ~/.config/fish
+git init
+git remote add origin https://github.com/bnw/fish-config.git
+git fetch
+git reset origin/master
+git ls-files -d | sed -e \"s/\(.*\)/'\1'/\" | xargs git checkout -- "
